82b00e1d4e1fd9dd94cbb747b499637dc0a1258b
Kenny Rachuonyo <krmboya@gmail.com>
2023-03-15 13:39:42 +0300

    deploy new version

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -6,25 +6,6 @@ const main = async () => {
     await domainContract.deployed();
 
     console.log("Contract deployed to:", domainContract.address);
-    
-    // register domain using some ether
-    let txn = await domainContract.register("aeterno", {value: hre.ethers.utils.parseEther("0.1")});
-    await txn.wait();
-    console.log("Minted domain aeterno.alfa");
-
-    // set a record for the domain
-    txn = await domainContract.setRecord("aeterno", "Was, is and forever will be");
-    await txn.wait();
-    console.log("Record set for aeterno.alfa");
-
-    // Get the owner of the domain
-    const address = await domainContract.getAddress("aeterno");
-    console.log("Owner of aeterno is:", address);
-
-    // Get the contract balance
-    const balance = await hre.ethers.provider.getBalance(domainContract.address);
-    console.log("Contract balance:", hre.ethers.utils.formatEther(balance));
-    
 };
 
 
